6709ccbfdaec61dd5c3862f67f59690593df7460
Funso Joba <42432746+funsojoba@users.noreply.github.com>
2021-07-19 17:09:16 +0100

    updated unit test

--- DRO_project/tests/test_cycle_event.py
+++ DRO_project/tests/test_cycle_event.py
@@ -35,3 +35,17 @@ class TestCreateCycle(TestCase):
         res = self.client.get(self.url + '?date='+date)
         self.assertEqual(res.data[0]['event'], "mentral cycle starts")
         self.assertEqual(res.data[0]['date'], date)
+
+    def test_valid_date_return_response(self):
+        cycle_date = PeriodCylceModel.objects.create(
+            last_period_date=datetime.date(2021, 6, 1),
+            cycle_average=25,
+            period_average=5,
+            start_date=datetime.date(2021, 7, 1),
+            end_date=datetime.date(2021, 12, 1),
+            total_created_cycle=5
+        )
+        cycle_date.save()
+        date = "2021-08-14"
+        res = self.client.get(self.url + '?date='+date)
+        self.assertEqual(res.status_code, 200)
